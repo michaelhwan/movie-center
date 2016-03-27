9731979652de6b9995396ab7b7598644c45ca703
Michael Hwan <michaelhwan@users.noreply.github.com>
2016-03-26 18:20:28 -0700

    add Python comments notation

--- media.py
+++ media.py
@@ -1,12 +1,13 @@
 import webbrowser
 
 class Movie():
-
+  
+  '''This class stores movie related information'''
   def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer):
     self.title = movie_title
     self.poster_image_url = poster_image
     self.trailer_youtube_url = youtube_trailer
 
-  # Opens a YouTube trailer within the browser without opening a new tab.
+  '''Opens a YouTube trailer within the browser without opening a new tab.'''
   def show_trailer(self):
     webbrowser.open(self.trailer_youtube_url)
