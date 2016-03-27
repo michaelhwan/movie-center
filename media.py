c693384a4cba3fd1ed9c022b1575e4cca3b0654d
Michael Hwan <michaelhwan@users.noreply.github.com>
2016-03-26 19:47:36 -0700

    change to 3 arguments instead of 4

--- media.py
+++ media.py
@@ -3,7 +3,7 @@ import webbrowser
 class Movie():
   
   '''This class stores movie related information'''
-  def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer):
+  def __init__(self, movie_title, poster_image, youtube_trailer):
     self.title = movie_title
     self.poster_image_url = poster_image
     self.trailer_youtube_url = youtube_trailer
